097bc29dce2cc011b962f99dcf6f2326bff5e46b
Clarus-Moof <moooof@me.com>
2026-04-20 18:18:00 +0200

    applemailmcp: update to v1.0.1

--- Formula/applemailmcp.rb
+++ Formula/applemailmcp.rb
@@ -1,9 +1,9 @@
 class Applemailmcp < Formula
   desc "Local MCP server for Apple Mail with Contacts integration"
   homepage "https://github.com/Clarus-Moof/AppleMailMCP"
-  url "https://github.com/Clarus-Moof/AppleMailMCP/releases/download/v1.0.0/AppleMailMCP-1.0.0.zip"
-  sha256 "0e47fb2a7a60912773900b1219fd5c4d558094f55772ae40aae291b8f40409d0"
-  version "1.0.0"
+  url "https://github.com/Clarus-Moof/AppleMailMCP/releases/download/v1.0.1/AppleMailMCP-1.0.1.zip"
+  sha256 "05570c6496e9ad457e48970da7db121e2ab33a12010ffbd873f1373128d75885"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
